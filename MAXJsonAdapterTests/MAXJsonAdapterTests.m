4d09b711ef3a57754301cee9c7281c671f45be12
roodoodey <mathieugrettir@hotmail.com>
2016-12-27 00:48:52 +0000

    Initial Commit

---
+++ MAXJsonAdapterTests/MAXJsonAdapterTests.m
@@ -0,0 +1,39 @@
+//
+//  MAXJsonAdapterTests.m
+//  MAXJsonAdapterTests
+//
+//  Created by Mathieu Skulason on 27/12/16.
+//  Copyright © 2016 Konta ehf. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MAXJsonAdapterTests : XCTestCase
+
+@end
+
+@implementation MAXJsonAdapterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
