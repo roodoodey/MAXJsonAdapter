525e93ca08aa3ad613bfd8a46f7604717f4cc70f
Mathieu Skulason <mathieugrettir@hotmail.com>
2017-11-15 02:44:17 +0000

    Added several new tests for subclassing a property with a delegate method so that the subclassed property can inherit all of the delegate functionality from the json adapter. Currently the tests only if it maps itself accordingly for both array type subclassed properties and single type.

--- MAXJsonAdapterTests/MAXJAObjectSubclassCreationTests.m
+++ MAXJsonAdapterTests/MAXJAObjectSubclassCreationTests.m
@@ -21,6 +21,14 @@
 
 @implementation MAXSingleContainedObject
 
+-(NSArray <MAXJsonAdapterPropertyMap *> *)MAXJAPropertiesToMapObjectCreation {
+    
+    return @[
+             [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"merchantName" nextPropertyMap: [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"merchant" nextPropertyMap: nil]],
+             [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"amount" nextPropertyMap: [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"transactionAmount" nextPropertyMap: nil]]
+             ];
+}
+
 @end
 
 #pragma mark Normal Subclassed Objects
@@ -42,6 +50,23 @@
 
 @end
 
+@interface MAXNormalRootObjectWithDelegate : NSObject <MAXJsonAdapterDelegate>
+
+@property (nonatomic, strong) NSString *name;
+
+@property (nonatomic, strong) MAXSingleContainedObject *transaction;
+
+@end
+
+@implementation MAXNormalRootObjectWithDelegate
+
+-(NSArray <MAXJASubclassedProperty *> *)MAXJASubclassedProperties {
+    
+    return @[[MAXJASubclassedProperty MAXJAPropertyKey: @"transaction" class: [MAXSingleContainedObject class] delegate: [MAXSingleContainedObject new]]];
+}
+
+@end
+
 @interface MAXNormalArrayRootObject: NSObject <MAXJsonAdapterDelegate>
 
 @property (nonatomic, strong) NSString *name;
@@ -55,6 +80,23 @@
 -(NSArray <MAXJASubclassedProperty *> *)MAXJASubclassedProperties {
     
     return @[[MAXJASubclassedProperty MAXJAPropertyKey: @"transactions" class: [MAXSingleContainedObject class] delegate: nil]];
+}
+
+@end
+
+@interface MAXNormalArrayRootObjectWithDelegate: NSObject <MAXJsonAdapterDelegate>
+
+@property (nonatomic, strong) NSString *name;
+
+@property (nonatomic, strong) NSArray <MAXSingleContainedObject *> *transactions;
+
+@end
+
+@implementation MAXNormalArrayRootObjectWithDelegate
+
+-(NSArray <MAXJASubclassedProperty *> *)MAXJASubclassedProperties {
+    
+    return @[[MAXJASubclassedProperty MAXJAPropertyKey: @"transactions" class: [MAXSingleContainedObject class] delegate: [MAXSingleContainedObject new]]];
 }
 
 @end
@@ -85,6 +127,31 @@
 -(NSArray <MAXJASubclassedProperty *> *)MAXJASubclassedProperties {
     
     return @[[MAXJASubclassedProperty MAXJAPropertyKey: @"containedObject" class: [MAXSingleContainedObject class] delegate: nil]];
+}
+
+@end
+
+@interface MAXKeyMappedRootObjectWithDelegate : NSObject <MAXJsonAdapterDelegate>
+
+@property (nonatomic, strong) NSString *mappedName;
+
+@property (nonatomic, strong) MAXSingleContainedObject *containedObject;
+
+@end
+
+@implementation MAXKeyMappedRootObjectWithDelegate
+
+-(NSArray <MAXJsonAdapterPropertyMap *> *)MAXJAPropertiesToMapObjectCreation {
+    
+    return @[
+             [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"containedObject" nextPropertyMap: [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"transaction" nextPropertyMap: nil]],
+             [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"mappedName" nextPropertyMap: [MAXJsonAdapterPropertyMap MAXJACreateMapWithNewKey: @"name" nextPropertyMap: nil]]
+             ];
+}
+
+-(NSArray <MAXJASubclassedProperty *> *)MAXJASubclassedProperties {
+    
+    return @[[MAXJASubclassedProperty MAXJAPropertyKey: @"containedObject" class: [MAXSingleContainedObject class] delegate: [MAXSingleContainedObject new]]];
 }
 
 @end
@@ -160,6 +227,35 @@
     
 }
 
+#pragma mark - Normal Object Subclass No Mapping With Sub-Delegate Mapping
+
+-(void)testNormalContainedObjectSubclassWithDelegateMapping {
+    
+    NSDictionary *dict = @{ @"name": @"Marius", @"transaction" : @{ @"merchant" : @"Hagkaup", @"transactionAmount" : @3000 } };
+    
+    MAXNormalRootObjectWithDelegate *rootObj = [MAXJsonAdapter MAXJACreateObjectOfClass: [MAXNormalRootObjectWithDelegate class] delegate: [MAXNormalRootObjectWithDelegate new] fromDictionary: dict];
+    
+    XCTAssertEqualObjects( rootObj.name, @"Marius");
+    XCTAssertEqualObjects( rootObj.transaction.merchantName, @"Hagkaup");
+    XCTAssertEqualObjects( rootObj.transaction.amount, @3000);
+    
+}
+
+-(void)testNormalContainedArrayObjectSubclassWithDelegateMapping {
+    
+    NSDictionary *dict =  @{ @"name": @"Marius", @"transactions" : @[ @{ @"merchant" : @"Hagkaup", @"transactionAmount" : @3000 }, @{ @"merchant" : @"Nói Síríus", @"transactionAmount" : @5000 } ] };
+    
+    MAXNormalArrayRootObjectWithDelegate *rootObj = [MAXJsonAdapter MAXJACreateObjectOfClass: [MAXNormalArrayRootObjectWithDelegate class] delegate: [MAXNormalArrayRootObjectWithDelegate new]  fromDictionary: dict];
+    
+    XCTAssertEqualObjects( rootObj.name, @"Marius");
+    XCTAssertEqualObjects( @(rootObj.transactions.count), @2);
+    XCTAssertEqualObjects( rootObj.transactions[0].merchantName, @"Hagkaup");
+    XCTAssertEqualObjects( rootObj.transactions[0].amount, @3000);
+    XCTAssertEqualObjects( rootObj.transactions[1].merchantName, @"Nói Síríus");
+    XCTAssertEqualObjects( rootObj.transactions[1].amount, @5000);
+    
+}
+
 #pragma mark - Normal Object Subclass With Mapping Without Sub-Delegate
 
 -(void)testNormalContainedObjectWithKeyMappingSubclassWithoutDelegate {
@@ -185,6 +281,20 @@
     XCTAssertEqualObjects( rootObj.containedObjects[0].amount, @3000);
     XCTAssertEqualObjects( rootObj.containedObjects[1].merchantName, @"Nói Síríus");
     XCTAssertEqualObjects( rootObj.containedObjects[1].amount, @5000);
+    
+}
+
+#pragma mark - Normal Object Subclass With Mapping and Sub-Delegate Mapping
+
+-(void)testNormalContainedObjectWithKeyMappingSubclassWithDelegate {
+    
+    NSDictionary *dict = @{ @"name": @"Marius", @"transaction" : @{ @"merchant" : @"Hagkaup", @"transactionAmount" : @3000 } };
+    
+    MAXKeyMappedRootObjectWithDelegate *rootObj = [MAXJsonAdapter MAXJACreateObjectOfClass: [MAXKeyMappedRootObjectWithDelegate class] delegate: [MAXKeyMappedRootObjectWithDelegate new] fromDictionary: dict];
+    
+    XCTAssertEqualObjects( rootObj.mappedName, @"Marius");
+    XCTAssertEqualObjects( rootObj.containedObject.merchantName, @"Hagkaup");
+    XCTAssertEqualObjects( rootObj.containedObject.amount, @3000);
     
 }
 
